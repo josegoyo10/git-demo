44896918b74bbfc7e41e766090919c0ec5fb9c2d
Jose Gregorio <jgregoriorason@gmail.com>
2017-10-30 12:24:09 -0300

    Create Department Model
    Add department funcionality

---
+++ app/Department.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+
+    	$this->name = $name;
+    }
+}
