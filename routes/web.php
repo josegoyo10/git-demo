353bccb530e6aa8e5dadb4771fb014989bbe352d
Jose Gregorio <jgregoriorason@gmail.com>
2017-10-30 10:35:41 -0300

    Create user view

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('users', function () {
+    return view('users');
+
+});
